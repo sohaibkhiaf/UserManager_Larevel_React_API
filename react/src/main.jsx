59d61c2e6d65a621b92b0a51200e7d72e2f93108
Sohaib Khiaf <sohaibkhiaf@gmail.com>
2026-01-01 00:29:37 +0100

    main

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -5,9 +5,6 @@ import router from './router.jsx'
 import { RouterProvider } from 'react-router-dom'
 import { ContextProvider } from './contexts/ContextProvider.jsx'
 
-//React + Laravel Full-stack Application | Build and Deploy
-// 2:32:44
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ContextProvider>
